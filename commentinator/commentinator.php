f6fd558446a3fb266f850d34f35f0529216b4e14
po_taka <angel.koilov@gmail.com>
2013-12-30 15:58:40 +0200

    initial commit

---
+++ commentinator/commentinator.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+  Plugin Name: Commentinator
+  Plugin URI: https://github.com/angelk/commentinator
+  Version: 0.1
+  Author: Angel Koilov
+  Description: @TODO
+ */
+
+function commntinatorCheckComment($commentdata)
+{
+    if (0 === preg_match('/[а-яА-Я]/', $commentdata['comment_content'])) {
+        wp_die('Коментарът трябва да съдържа поне един символ на кирилица');
+    }
+    return $commentdata;
+}
+
+add_action('preprocess_comment', 'commntinatorCheckComment');
